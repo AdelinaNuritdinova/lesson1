46f6d79f85c532d756390789ab07e6eca5bb8051
Adelina <adelya.n.01@gmail.com>
2019-06-24 14:47:34 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final float pi = 3.14f;
+        final String myConstantValue = "Adelina";
+
+        String name = "Adelina";
+        System.out.println(name);
+
+        int myNumber = 45;
+        System.out.println(myNumber);
+        myNumber = 23;
+        System.out.println("Значение переменной - " + myNumber + "текст" + 66);
+
+        System.out.println("Hello world!");
+        System.out.println("Привет мир!");
+        System.out.println("19 june");
+        System.out.println(7);
+
+
+
+    }
+}
